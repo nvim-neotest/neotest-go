992cf8a9518177488c11719014071bde32d84eef
lunarxlark <lunarxlark@gmail.com>
2022-06-16 00:23:56 +0900

    colorize output

--- lua/neotest-go/init.lua
+++ lua/neotest-go/init.lua
@@ -27,6 +27,13 @@ local function sanitize_output(output)
   if not output then
     return output
   end
+  if string.find(output, 'FAIL') then
+    output = output:gsub("^", "[31m"):gsub("$", "[0m")
+  elseif string.find(output, 'PASS') then
+    output = output:gsub("^", "[32m"):gsub("$", "[0m")
+  elseif string.find(output, 'SKIP') then
+    output = output:gsub("^", "[33m"):gsub("$", "[0m")
+  end
   return output:gsub('\n', ''):gsub('\t', '')
 end
 
