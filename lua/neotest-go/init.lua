75b54ac346c58b1181956750930af18957f99e6b
Giovan Isa Musthofa <giovanmail@gmail.com>
2022-06-28 02:44:45 +0700

    Improve test table tree sitter query

--- lua/neotest-go/init.lua
+++ lua/neotest-go/init.lua
@@ -191,21 +191,37 @@ function adapter.discover_positions(path)
   if get_experimental_opts().test_table then
     query = query .. [[
 
-    (short_var_declaration
-      left: (expression_list
-        (identifier) @table.name)
-        (#match? @table.name "test")
-      right: (expression_list
-        (composite_literal
-          (literal_value
-            (literal_element
-              (literal_value
-                (keyed_element
-                  (literal_element
-                    (identifier) @test.field.name
-                    (#match? @test.field.name "name|desc"))
-                  (literal_element
-                    (interpreted_string_literal) @test.name)))) @test.definition ))))
+    (block
+      (short_var_declaration
+        left: (expression_list
+          (identifier) @test.cases)
+        right: (expression_list
+          (composite_literal
+            (literal_value
+              (literal_element
+                (literal_value
+                  (keyed_element
+                    (literal_element
+                      (identifier) @test.field.name)
+                    (literal_element
+                      (interpreted_string_literal) @test.name)))) @test.definition))))
+      (for_statement
+        (range_clause
+          left: (expression_list
+            (identifier) @test.case)
+          right: (identifier) @test.cases1
+            (#eq? @test.cases @test.cases1))
+        body: (block
+          (call_expression
+            function: (selector_expression
+              field: (field_identifier) @test.method)
+              (#match? @test.method "^Run$")
+            arguments: (argument_list
+              (selector_expression
+                operand: (identifier) @test.case1
+                (#eq? @test.case @test.case1)
+                field: (field_identifier) @test.field.name1
+                (#eq? @test.field.name @test.field.name1)))))))
     ]]
   end
 
