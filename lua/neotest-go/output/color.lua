49d24f1fa32d470bf7c1d8587e512b32e7010c0b
Davincible <david.brouwer.99@gmail.com>
2022-10-26 05:51:50 +0200

    oops

--- lua/neotest-go/output/color.lua
+++ lua/neotest-go/output/color.lua
@@ -6,16 +6,12 @@ local colors_patterns = require('neotest-go.patterns').colors
 local term = os.getenv('COLORTERM')
 local fullcolor = (term == 'truecolor') or (term == '24bit')
 
-local function is_table(a)
-  return type(a) == 'table'
-end
-
 local function slice(tbl, first, last, step)
-  local sliced = {}
-  for i = first or 1, last or #tbl, step or 1 do
-    sliced[#sliced + 1] = tbl[i]
-  end
-  return sliced
+	local sliced = {}
+	for i = first or 1, last or #tbl, step or 1 do
+		sliced[#sliced + 1] = tbl[i]
+	end
+	return sliced
 end
 
 -- Function:	HEXtoRGB(arg)
@@ -27,19 +23,19 @@ end
 -- 						Blue value from 0-255
 -- Source:    forum.rainmeter.net/viewtopic.php?t=29419
 local function HEXtoRGB(hexArg)
-  hexArg = hexArg:gsub('#', '')
+	hexArg = hexArg:gsub('#', '')
 
-  if string.len(hexArg) == 3 then
-    return tonumber('0x' .. hexArg:sub(1, 1)) * 17,
-      tonumber('0x' .. hexArg:sub(2, 2)) * 17,
-      tonumber('0x' .. hexArg:sub(3, 3)) * 17
-  elseif string.len(hexArg) == 6 then
-    return tonumber('0x' .. hexArg:sub(1, 2)),
-      tonumber('0x' .. hexArg:sub(3, 4)),
-      tonumber('0x' .. hexArg:sub(5, 6))
-  else
-    return 0, 0, 0
-  end
+	if string.len(hexArg) == 3 then
+		return tonumber('0x' .. hexArg:sub(1, 1)) * 17,
+				tonumber('0x' .. hexArg:sub(2, 2)) * 17,
+				tonumber('0x' .. hexArg:sub(3, 3)) * 17
+	elseif string.len(hexArg) == 6 then
+		return tonumber('0x' .. hexArg:sub(1, 2)),
+				tonumber('0x' .. hexArg:sub(3, 4)),
+				tonumber('0x' .. hexArg:sub(5, 6))
+	else
+		return 0, 0, 0
+	end
 end
 
 -- Get color from config, of default if not present in config.
@@ -48,18 +44,17 @@ end
 -- @param color_class string
 -- @return string
 local function get_color(config, section, index, color_class)
-  local color = colors_patterns[section][color_class]
-  if
-    config[section] ~= nil
-    or config[section][color_class] ~= nil and #config[section][color_class] ~= 0
-  then
-    color = config[section][color_class]
-  end
+	local color = colors_patterns[section][color_class]
+	if config[section] ~= nil
+			or config[section][color_class] ~= nil and #config[section][color_class] ~= 0
+	then
+		color = config[section][color_class]
+	end
 
-  if is_table(color) then
-    return color
-  end
-  return color[index]
+	if type(color) ~= 'table' then
+		return color
+	end
+	return color[index]
 end
 
 -- Add 24bit console color to a line.
@@ -67,10 +62,10 @@ end
 -- @param hex_color string
 -- @return string
 local function add_24bit_color(line, hex_color)
-  local r, g, b = HEXtoRGB(hex_color)
-  local start_code = fmt('[38;2;%d;%d;%dm', r, g, b)
-  local end_code = '[0m'
-  return start_code .. line .. end_code
+	local r, g, b = HEXtoRGB(hex_color)
+	local start_code = fmt('[38;2;%d;%d;%dm', r, g, b)
+	local end_code = '[0m'
+	return start_code .. line .. end_code
 end
 
 -- Add 8bit term color to a line.
@@ -78,9 +73,9 @@ end
 --@param color string
 --@return string
 local function add_8bit_color(line, color)
-  local start_code = fmt('[%dm', color)
-  local end_code = '[0m'
-  return start_code .. line .. end_code
+	local start_code = fmt('[%dm', color)
+	local end_code = '[0m'
+	return start_code .. line .. end_code
 end
 
 -- Colorize a line.
@@ -90,12 +85,16 @@ end
 -- @return string
 -- colorized = add_color(name, match_group, config, i)
 local function add_color(section, line, config, i)
-  if fullcolor then
-    local color = get_color(config, section, i, 'gui')
-    return add_24bit_color(line, color)
-  end
-  local color = get_color(config, section, i, 'term')
-  return add_8bit_color(line, color)
+	if fullcolor then
+		local color = get_color(config, section, i, 'gui')
+		return add_24bit_color(line, color)
+	end
+	local color = get_color(config, section, i, 'term')
+	return add_8bit_color(line, color)
+end
+
+local function is_table(a)
+	return type(a) == 'table'
 end
 
 -- Highlight output.
@@ -103,30 +102,30 @@ end
 -- @param config table
 -- @return string
 function M.highlight_output(output, opts)
-  if not output then
-    return output
-  end
+	if not output then
+		return output
+	end
 
-  local config = colors_patterns
-  if is_table(opts) and opts.colors ~= nil then
-    config = opts.colors
-  end
+	local config = colors_patterns
+	if is_table(opts) and opts.colors ~= nil then
+		config = opts.colors
+	end
 
-  -- Itterate over all patterns to colorize
-  for name, c in pairs(colors_patterns) do
-    local res = { string.find(output, c.pattern) }
-    if #res > 2 then
-      -- Colorize all groups in pattern
-      for i, match_group in ipairs(slice(res, 3)) do
-        local colorized = add_color(name, match_group, config, i)
-        output = string.gsub(output, match_group, colorized)
-      end
-    elseif #res > 0 then
-      output = add_color(name, output, config)
-    end
-  end
+	-- Itterate over all patterns to colorize
+	for name, c in pairs(colors_patterns) do
+		local res = { string.find(output, c.pattern) }
+		if #res > 2 then
+			-- Colorize all groups in pattern
+			for i, match_group in ipairs(slice(res, 3)) do
+				local colorized = add_color(name, match_group, config, i)
+				output = string.gsub(output, match_group, colorized)
+			end
+		elseif #res > 0 then
+			output = add_color(name, output, config)
+		end
+	end
 
-  return output
+	return output
 end
 
 return M
