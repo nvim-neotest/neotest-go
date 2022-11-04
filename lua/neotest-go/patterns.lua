7b8c59e192747d36bb9d1125f0dd936db4134974
Davincible <david.brouwer.99@gmail.com>
2022-11-04 15:58:32 +0100

    small pattern improvement

--- lua/neotest-go/patterns.lua
+++ lua/neotest-go/patterns.lua
@@ -4,8 +4,8 @@ local patterns = {
   error = { "error" },
   -- Patterns to colorize in output
   colors = {
-    run = { pattern = "(===%s+RUN)%s+(.*)", gui = { "#777777", "#deb887" }, term = { 34, 34 } },
-    file = { pattern = "^%s+(.*.go):(%d+):", gui = { "#20b2aa", "#a474dc" }, term = { 36, 35 } },
+    run = { pattern = "^(===%s+RUN)%s+(.*)", gui = { "#777777", "#deb887" }, term = { 34, 34 } },
+    file = { pattern = "^%s+(.*.go)(:%d+):", gui = { "#20b2aa", "#a474dc" }, term = { 36, 35 } },
     pass = { pattern = "^---%s+PASS:", gui = "#14D000", term = 31 },
     fail = { pattern = "^---%s+FAIL:", gui = "#cc0000", term = 32 },
     skip = { pattern = "^---%s+SKIP:", gui = "#729fcf", term = 33 },
